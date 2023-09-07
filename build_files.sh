22f38f31728df4b2653172004f8635808a771a2e
Aakash Mishra <aakashmishra.281@gmail.com>
2023-09-07 16:05:21 +0530

    updated buildfiles

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,8 @@
-echo " BUILD START"
+echo "creating a virtual environment"
+python -m venv venv
+source venv/bin/activate
+python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput --clear
-echo " BUILD ENDS"
